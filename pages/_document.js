eac9eb83513d20852e5860705e8ddcd5848de350
Pizatol <dev.belmonte@gmail.com>
2022-04-10 17:07:42 +0200

    premier commit

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&family=Roboto:wght@500&display=swap"
+                    rel="stylesheet"
+                />
+
+
+            <body>
+                <Main />
+               
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
---
+++ public/assets/icons/Icons.js
@@ -0,0 +1,46 @@
+
+
+
+export const icons = [
+	{
+		id : 1,
+		url : "/../assets/icons/html.svg",
+		name : "html icon"
+	},
+	{
+		id : 2,
+		url : "/../assets/icons/css.svg",
+		name : "css icon"
+	},
+	{
+		id : 3,
+		url : "/../assets/icons/js.svg",
+		name : "javascript icon"
+	},
+	{
+		id : 4,
+		url : "/../assets/icons/react.svg",
+		name : "react icon"
+	},
+	{
+		id : 5,
+		url : "/../assets/icons/next.svg",
+		name : "next icon"
+	},
+	{
+		id : 6,
+		url : "/../assets/icons/firebase.svg",
+		name : "firebase icon"
+	},
+	{
+		id : 7,
+		url : "/../assets/icons/sass.svg",
+		name : "sass icon"
+	},
+	{
+		id : 8,
+		url : "/../assets/icons/gsap.svg",
+		name : "greensock icon"
+	},
+
+]
